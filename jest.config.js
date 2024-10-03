09b870364adc26fa2a704b99630c960126d82aa1
Victor Dell <victorvinicius33.vv@gmail.com>
2024-10-03 08:31:47 -0300

    refactor: change the jest.config archive type to javascript

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+}
